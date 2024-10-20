4aba2ff9e5396f487ba5baa5dc0c801c2ad6b1a1
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-10-20 08:58:06 -0700

    Added new no camera image

--- src/components/videocall/UserGrid.tsx
+++ src/components/videocall/UserGrid.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LocalUser, RemoteUser } from "agora-rtc-react";
 import Image from "next/image";
 import { IAgoraRTCRemoteUser, ICameraVideoTrack } from "agora-rtc-react";
+import noCamera from "../../../public/images/noCamera.png";
 
 interface UserGridProps {
   localCameraTrack: ICameraVideoTrack | null;
@@ -46,28 +47,22 @@ const UserGrid: React.FC<UserGridProps> = ({
           key={user.uid || index}
           className="relative rounded-lg overflow-hidden w-full"
         >
-          {user.uid === "local" && cameraOn && localCameraTrack ? (
+          {user.uid === "local" && localCameraTrack ? (
             <LocalUser
               cameraOn={cameraOn}
               micOn={micOn}
               videoTrack={localCameraTrack}
-              cover="/images/cover.jpg"
+              cover={noCamera.src}
               className="w-full h-full"
             />
           ) : user.uid !== "local" ? (
             <RemoteUser
               user={user as IAgoraRTCRemoteUser}
-              cover="/images/cover.jpg"
+              cover={noCamera.src}
               className="w-full h-full"
             />
           ) : (
-            <Image
-              src="/images/cover.jpg"
-              alt="You"
-              className="w-full h-auto"
-              width={100}
-              height={100}
-            />
+            <Image src={noCamera} alt="You" className="w-full h-full" />
           )}
           <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 px-2 py-1 rounded text-white text-sm">
             {user.uid === "local" ? "You" : `User ${user.uid}`}
