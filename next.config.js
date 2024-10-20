a5bd7d6032d455771fae12905ad1ccba88851d1d
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-10-20 10:43:27 -0700

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
